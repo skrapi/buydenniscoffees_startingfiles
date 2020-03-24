a6f9d26e0ed408f646216b593ef0b121fe19e12d
Sylvan <sylvan.j.morris@gmail.com>
2020-03-24 21:09:49 +0200

    Cleaned out the repo, first learning django

--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('charge/', views.charge, name="charge"),
-    path('success/<str:args>/', views.successMsg, name="success"),
-]
